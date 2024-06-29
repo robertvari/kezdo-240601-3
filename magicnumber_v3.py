c209af0a8d9d62f00cbf5c9a601a8c2c48af83a5
Robert Vari <mail.robertvari@gmail.com>
2024-06-29 16:53:42 +0200

    update

--- magicnumber_v3.py
+++ magicnumber_v3.py
@@ -9,6 +9,11 @@ class MagicNumber:
 
         self.clear_screen()
         self.intro()
+
+        if self.__player.ask_for_start_game():
+            print("Start game...")
+        else:
+            self.exit_game()
     
     def clear_screen(self):
         os.system("cls")
@@ -22,6 +27,9 @@ class MagicNumber:
         print("I give you 30 credits to start. If you guess right you win 10 credits.")
         print("Be careful, you could lose all your credits ;)")
 
+    def exit_game(self):
+        self.clear_screen()
+        print("Sorry to see you go :( Maybe next time ;)")
 
 class Player:
     def __init__(self):
@@ -35,6 +43,18 @@ class Player:
 
     def get_name(self):
         self.__name = input("What is your name?")
+
+    def ask_for_start_game(self):
+        result = input("Are you ready? (y/n)")
+        if result == "y":
+            return True
+        return False
+    
+    def ask_for_new_round(self):
+        result = input("Do you want to play again? (y/n)")
+        if result == "y":
+            return True
+        return False
 
 class Computer:
     def __init__(self):
