51af21c05fbf8bc6df4aad1f37998a4839eeb113
Robert Vari <mail.robertvari@gmail.com>
2024-06-30 14:59:44 +0200

    update

--- Blackjack/game_assets/cards.py
+++ Blackjack/game_assets/cards.py
@@ -26,7 +26,25 @@ class Deck:
         self.reset()
 
     def reset(self):
-        self.__cards.append("Hello")
+        self.__cards.clear()
+
+        cards = [
+            ["2", 2],
+            ["3", 3],
+            ["4", 4],
+            ["5", 5],
+            ["6", 6],
+            ["7", 7],
+            ["8", 8],
+            ["9", 9],
+            ["10", 10],
+            ["King", 10],
+            ["Queen", 10],
+            ["Jack", 10],
+            ["Ace", 11]
+        ]
+
+        suits = ["Heart", "Club", "Diamond", "Spade"]
     
     def draw(self):
         pass
