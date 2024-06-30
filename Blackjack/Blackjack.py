e2d02752457e34ba802a45cd6993eae64d21e293
Robert Vari <mail.robertvari@gmail.com>
2024-06-30 14:26:48 +0200

    update

---
+++ Blackjack/Blackjack.py
@@ -0,0 +1,20 @@
+import os
+
+class Blackjack:
+    def __init__(self):
+        self.__clear_screen()
+        self.__intro()
+
+    def __clear_screen(self):
+        os.system("cls")
+
+    def __intro(self):
+        print("*"*50, "BLACKJACK", "*"*50)
+        print("This is a simple card game. Your only goal is to collect cards till your hand's value reaches 21.")
+
+    def __exit_game(self):
+        self.__clear_screen()
+        print("See you later! :)")
+        exit()
+
+Blackjack()
