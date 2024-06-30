5c55d496296ed47437d9e81be88c2609457db338
Robert Vari <mail.robertvari@gmail.com>
2024-06-30 17:28:28 +0200

    update

--- Blackjack/game_assets/players.py
+++ Blackjack/game_assets/players.py
@@ -28,6 +28,27 @@ class Player_BASE:
     def hand_value(self):
         return sum([card.value for card in self.__hand])
 
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def playing(self):
+        return self.__playing
+    
+    @playing.setter
+    def playing(self, value):
+        self.__playing = value
+
+    @property
+    def cards(self):
+        return str(self.__hand)
+    
+    def _add_card(self, new_card):
+        if self.hand_value > 10 and new_card.value == 11:
+            new_card.change_ace()
+        self.__hand.append(new_card)
+
     def init_hand(self, deck):
         self.__hand.clear()
 
@@ -63,6 +84,22 @@ class Player(Player_BASE):
         # self._set_name(input("What is your name? "))
         self._set_name("Robert Vari")
 
+    def draw(self, deck):
+        print(f"This is your turn {self.name}")
+
+        while self.playing:
+            print(f"Your cards: {self.cards}")
+            print(f"Your hand value: {self.hand_value}")
+
+            response = input("Do you want to draw a card? (y/n)")
+
+            if response == "y":
+                new_card = deck.draw()
+                print(f"Your new cards: {new_card}")
+                self._add_card(new_card)
+            else:
+                self.playing = False
+
 class AIPlayer(Player_BASE):
     pass
 
@@ -72,21 +109,15 @@ if __name__ == '__main__':
     deck = Deck()
 
     ai_player1 = AIPlayer()
-    ai_player2 = AIPlayer()
-    ai_player3 = AIPlayer()
     player = Player()
 
+    player.cards.append("Hello!!!")
+
     ai_player1.init_hand(deck)
-    ai_player2.init_hand(deck)
-    ai_player3.init_hand(deck)
     player.init_hand(deck)
-    
+
     ai_player1.draw(deck)
-    ai_player2.draw(deck)
-    ai_player3.draw(deck)
     player.draw(deck)
 
     ai_player1.show_hand()
-    ai_player2.show_hand()
-    ai_player3.show_hand()
     player.show_hand()
